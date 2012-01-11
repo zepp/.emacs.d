133ca04a36896ec2bb29ef14dfc33f2fa2ea12df
Pavel Sokolov <sokolov.p.a@gmail.com>
2012-01-11 01:23:01 -0800

    gtags: suggested key mapping is forced

--- cfg/gtags.el
+++ cfg/gtags.el
@@ -1,6 +1,8 @@
 ;; depends on cc-mode because c-mode-map defined there
 (require 'cc-mode)
 
+(setq gtags-suggested-key-mapping t)
+
 (require 'gtags)
 
 (define-key c-mode-base-map (kbd "M-,")   'gtags-find-tag-from-here)
