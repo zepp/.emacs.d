f2c7c730d0cdd6c5dc3a2d76414892f6a34eef4d
Pavel Sokolov <sokolov.p.a@gmail.com>
2012-01-16 14:28:54 +0400

    keys: anticlockwise windows cycling

--- cfg/keys.el
+++ cfg/keys.el
@@ -1,11 +1,7 @@
 ;;-------------------------------------------------------------------------------
 ;; global keys definition
 
-;; (global-set-key (kbd "M-o") 'other-window)
-;; (global-set-key (kbd "M-0") 'delete-window)
-;; (global-set-key (kbd "M-1") 'delete-other-windows)
-;; (global-set-key (kbd "M-2") 'split-window-vertically)
-;; (global-set-key (kbd "M-3") 'split-window-horizontally)
+(global-set-key (kbd "C-x M-o") #'(lambda () (interactive) (other-window -1)))
 (global-set-key (kbd "M-C") 'previous-buffer)
 (global-set-key (kbd "M-c") 'next-buffer)
 (global-set-key (kbd "M-t") 'dabbrev-completion)
