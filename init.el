9e9cdeea800ad2675b07f9165fb774bb3325d789
Pavel Sokolov <pavel.zepp@gmail.com>
2012-08-02 20:12:39 +0400

    server: hack to distinguish client and initial frames

--- init.el
+++ init.el
@@ -184,6 +184,7 @@
 ;;-------------------------------------------------------------------------------
 
 (when (not (string= system-type "windows-nt"))
+  (add-to-list 'initial-frame-alist '(name . "emacs-initial"))
   (add-to-list 'default-frame-alist '(name . "emacs-client"))
   (add-to-list 'default-frame-alist '(font . "terminus-14"))
   (add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
