a9c36a198f5ea59de7a63f71387400947f1a8ce7
Pavel Sokolov <pavel.zepp@gmail.com>
2012-08-02 19:30:08 +0400

    wm: frame create/raise macro improved

--- loadable/wm.el
+++ loadable/wm.el
@@ -1,15 +1,16 @@
-(defmacro wm-generic-raise(name buf-or-fun start-fun)
+(defmacro wm-generic-raise(name start-fun &optional buf-or-fun)
   (let* ((frm-name (concat "emacs-" name))
 	 (frm-pred `(lambda (frame)
-			(string= ,frm-name (frame-parameter frame 'name))))
+		      (string= ,frm-name (frame-parameter frame 'name))))
 	 (frame-var-name (gensym)))
     `(let ((,frame-var-name (find-if ,frm-pred (frame-list))))
        (if ,frame-var-name
-	   (progn
-	     (select-frame-set-input-focus ,frame-var-name)
-	     (if (functionp ,buf-or-fun)
-		 (funcall ,buf-or-fun)
-	       (switch-to-buffer ,buf-or-fun)))
+	   ,(when buf-or-fun
+	      `(progn
+		 (select-frame-set-input-focus ,frame-var-name)
+		 (if (functionp ,buf-or-fun)
+		     (funcall ,buf-or-fun)
+		   (switch-to-buffer ,buf-or-fun))))
 	 (progn
 	   (message "making new frame for %s" ,name)
 	   (select-frame-set-input-focus (make-frame '((window-system . x)
@@ -18,13 +19,9 @@
 
 (defun wm-raise-jabber()
   (when (boundp 'jabber-roster-buffer)
-    (wm-generic-raise "jabber" #'jabber-switch-to-roster-buffer
-		      #'jabber-connect-with-secrets)))
-
+    (wm-generic-raise "jabber" #'jabber-connect-with-secrets)))
 
 (defun wm-raise-mail()
   (when (boundp 'wl-folder-buffer-name)
-    (wm-generic-raise "mail" #'(lambda ()
-				 (switch-to-buffer wl-folder-buffer-name)
-				 (wl-folder-check-all)) 
-		      #'wl)))
+    (wm-generic-raise "mail" #'wl)))
+
