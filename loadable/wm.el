872c59fedafa207ccace8bf0e741ef5dff90f024
Pavel Sokolov <pavel.zepp@gmail.com>
2012-08-08 01:15:13 +0400

    wm: run-raise macro is reworked to provide much cleaner code

--- loadable/wm.el
+++ loadable/wm.el
@@ -1,23 +1,26 @@
 (defmacro gen-run-raise(name run-fun &optional buf-or-fun)
-  (let* ((disp-evar (getenv "DISPLAY"))
-	 (frm-name (concat "emacs-" name))
-	 (frm-pred `(lambda (frame)
-		      (string= ,frm-name (frame-parameter frame 'name))))
-	 (frame-var-name (gensym)))
-    `(let ((,frame-var-name (find-if ,frm-pred (frame-list))))
-       (if ,frame-var-name
-	   ,(when buf-or-fun
-	      `(progn
+  (let* ((frm-name (concat "emacs-" name))
+	 (frame-var-name (gensym))
+	 (do-a-frame `(progn
+			(message "making new frame for %s" ,name)
+			(select-frame-set-input-focus 
+			 (make-frame '((window-system . x)
+				       (display . ,(getenv "DISPLAY"))
+				       (name . ,frm-name))))
+			(funcall ,run-fun))))
+    (if buf-or-fun
+	`(let ((,frame-var-name (find-if (lambda (frame)
+					   (string= ,frm-name
+						    (frame-parameter frame 'name)))
+					 (frame-list))))
+	   (if ,frame-var-name
+	       (progn
 		 (select-frame-set-input-focus ,frame-var-name)
 		 (if (functionp ,buf-or-fun)
 		     (funcall ,buf-or-fun)
-		   (switch-to-buffer ,buf-or-fun))))
-	 (progn
-	   (message "making new frame for %s" ,name)
-	   (select-frame-set-input-focus (make-frame '((window-system . x)
-						       (display . ,disp-evar)
-						       (name . ,frm-name))))
-	   (funcall ,run-fun))))))
+		   (switch-to-buffer ,buf-or-fun)))
+	     ,do-a-frame))
+      do-a-frame)))
 
 (defun wm-run-jabber()
   (when (boundp 'jabber-roster-buffer)
