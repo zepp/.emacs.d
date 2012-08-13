49ec4067b93cb9984217a84d5a03111579f1fd9e
Pavel Sokolov <pavel.zepp@gmail.com>
2012-08-13 16:03:37 +0400

    sr: pane location faces update

--- cfg/sr.el
+++ cfg/sr.el
@@ -3,3 +3,7 @@
 (require 'sunrise-commander)
 
 (setq sr-avfs-root "~/.avfs")
+
+(custom-set-faces
+ '(sr-active-path-face ((t (:background "dark" :foreground "orange" :weight bold :height 160))))
+ '(sr-passive-path-face ((t (:background "white" :foreground "grey" :weight bold :height 160)))))
