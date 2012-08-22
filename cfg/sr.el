5654e22c457d5eacafc4c175a3006355bffa816d
Pavel Sokolov <pavel.zepp@gmail.com>
2012-08-22 18:07:54 +0400

    sr: fun to run avfs in debug mode

--- cfg/sr.el
+++ cfg/sr.el
@@ -12,3 +12,12 @@
 (custom-set-faces
  '(sr-active-path-face ((t (:background "dark" :foreground "orange" :weight bold :height 160))))
  '(sr-passive-path-face ((t (:background "white" :foreground "grey" :weight bold :height 160)))))
+
+(defun sr-run-avfsd ()
+  (interactive)
+  ;; -d   enable debug output (implies -f)
+  ;; -f   foreground operation
+  ;; -s   disable multi-threaded operation
+  (async-shell-command "/usr/bin/avfsd -f -d -s -o intr -o sync_read \
+		       -o allow_root ~/.avfs/mount 2>&1" "*avfsd*"))
+
