911e838af17b329d3f9b26ddae36924c29dec594
Pavel Sokolov <pavel.zepp@gmail.com>
2012-08-23 13:33:46 +0400

    server: file name fixed

--- cfg/server.el
+++ cfg/server.el
@@ -4,6 +4,6 @@
 (add-hook 'kill-emacs-hook #'(lambda ()
 			       (with-current-buffer "*Messages*"
 				 (setq buffer-file-name
-				       (expand-file-name (buffer-name) 
+				       (expand-file-name "messages"
 							 my-emacs-var-dir))
 				 (basic-save-buffer))))
