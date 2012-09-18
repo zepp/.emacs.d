08b4506e24990d3c870763324ce6773322c163d6
Pavel Sokolov <pavel.zepp@gmail.com>
2012-09-18 18:29:23 +0400

    keys: updated

--- cfg/keys.el
+++ cfg/keys.el
@@ -1,15 +1,22 @@
 ;;-------------------------------------------------------------------------------
 ;; global keys definition
 
+(global-set-key (kbd "M-#") 'dictem-run-define)
+(global-set-key (kbd "M-t") 'dabbrev-completion)
+(global-set-key (kbd "C-t") 'nil)
+(global-set-key (kbd "C-x ^") 'iresize-mode)
 (global-set-key (kbd "C-x M-o") #'(lambda () (interactive) (other-window -1)))
 (global-set-key (kbd "C-x p") #'previous-buffer)
 (global-set-key (kbd "C-x n") #'next-buffer)
-(global-set-key (kbd "M-t") 'dabbrev-completion)
-(global-set-key (kbd "C-t") 'nil)
-(global-set-key (kbd "M-#") 'dictem-run-define)
-(global-set-key (kbd "C-c C-r") 'rgrep)
-(global-set-key (kbd "C-x M-f") 'find-file-other-window)
-(global-set-key (kbd "C-x ^") 'iresize-mode)
+(global-set-key (kbd "C-x M-f") #'find-file-other-window)
+(global-set-key (kbd "C-x M-b") #'switch-to-buffer-other-window)
+(global-set-key (kbd "C-x d") #'dired-jump)
+(global-set-key (kbd "C-x C-d") #'dired)
+(global-set-key (kbd "C-x M-d") #'dired-other-window)
+(global-set-key (kbd "C-c C-g") #'rgrep)
+(global-set-key (kbd "C-c S") #'split-window-vertically)
+(global-set-key (kbd "C-c s") #'split-window-horizontally)
+(global-set-key (kbd "C-c q") #'delete-other-windows)
 
 ;;-------------------------------------------------------------------------------
 ;; related to global key redefinition so placed here
