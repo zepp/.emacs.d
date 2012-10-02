cca2d5a64fd4ec6e89e8d0e88e9a38aac2e5bd74
Pavel Sokolov <pavel.zepp@gmail.com>
2012-10-02 23:17:45 +0400

    org: agenda file list is updated

--- cfg/org.el
+++ cfg/org.el
@@ -3,6 +3,6 @@
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org\\(-mode\\)?$" . org-mode))
 
-(setq org-agenda-files '("~/notes/agenda-private.org.gpg" "~/notes/agenda.org"))
+(setq org-agenda-files '("~/dropbox/org/agenda.org.gpg"))
 
 ;;-------------------------------------------------------------------------------
