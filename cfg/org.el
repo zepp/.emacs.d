0a727ec53a61819d87634cd1173bc05c4ce83ded
Pavel Sokolov <pavel.zepp@gmail.com>
2012-10-12 13:48:39 +0400

    org: key map update

--- cfg/org.el
+++ cfg/org.el
@@ -9,6 +9,7 @@
 (add-hook 'org-mode-hook #'(lambda () 
 			     ;; C-t is reserved by the stumpWM so this
 			     ;; binding will be very handy
-			     (define-key org-mode-map (kbd "C-c t") #'org-todo)))
+			     (define-key org-mode-map (kbd "C-c t") #'org-todo)
+			     (define-key org-agenda-mode-map (kbd "C-c t") #'org-agenda-todo)))
 
 ;;-------------------------------------------------------------------------------
