f6b87c032bab92b9a53e471caa8dbaa58e99f101
Pavel Sokolov <psokolov@synopsys.com>
2012-10-31 12:27:22 +0400

    gtags: after update hook
    update information about the file associated with the current buffer
    when one is saved to disk

--- cfg/gtags.el
+++ cfg/gtags.el
@@ -8,3 +8,20 @@
 (define-key c-mode-base-map (kbd "M-,")   'gtags-find-tag-from-here)
 (define-key c-mode-base-map (kbd "C-M-,") 'gtags-find-rtag)
 (define-key c-mode-base-map (kbd "M-/")   'gtags-find-pattern)
+
+(defun gtags-update-current-file (root)
+  (interactive)
+  (let ((rel-file-name (substring
+			(buffer-file-name
+			 (current-buffer)) (length root)))
+	(default-directory root))
+    (start-process "update-gtags" "update-gtags"
+		   shell-file-name shell-command-switch
+		   (format "gtags --single-update %s" rel-file-name))))
+
+(defun gtags-update-hook()
+  "Update GTAGS file incrementally upon saving a file"
+  (when (and gtags-mode gtags-rootdir)
+    (gtags-update-current-file gtags-rootdir)))
+
+(add-hook 'after-save-hook 'gtags-update-hook)
