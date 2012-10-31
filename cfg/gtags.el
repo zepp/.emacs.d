64185f2693dc98dc1c4c420f9a50dd13935db8c8
Pavel Sokolov <psokolov@synopsys.com>
2012-10-31 12:45:27 +0400

    gtags: my hack is removed
    similar functionality is provided by the gtags.el from the new package

--- cfg/gtags.el
+++ cfg/gtags.el
@@ -1,7 +1,8 @@
 ;; depends on cc-mode because c-mode-map defined there
 (require 'cc-mode)
 
-(setq gtags-suggested-key-mapping t)
+(setq gtags-suggested-key-mapping t
+      gtags-auto-update t)
 
 (require 'gtags)
 
@@ -9,19 +10,3 @@
 (define-key c-mode-base-map (kbd "C-M-,") 'gtags-find-rtag)
 (define-key c-mode-base-map (kbd "M-/")   'gtags-find-pattern)
 
-(defun gtags-update-current-file (root)
-  (interactive)
-  (let ((rel-file-name (substring
-			(buffer-file-name
-			 (current-buffer)) (length root)))
-	(default-directory root))
-    (start-process "update-gtags" "update-gtags"
-		   shell-file-name shell-command-switch
-		   (format "gtags --single-update %s" rel-file-name))))
-
-(defun gtags-update-hook()
-  "Update GTAGS file incrementally upon saving a file"
-  (when (and gtags-mode gtags-rootdir)
-    (gtags-update-current-file gtags-rootdir)))
-
-(add-hook 'after-save-hook 'gtags-update-hook)
