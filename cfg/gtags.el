ded01a957a55de3fd4b9c63929295614a0a112a7
Pavel Sokolov <psokolov@synopsys.com>
2012-11-02 15:28:41 +0400

    gtags: mode map is reworked, no suggestions

--- cfg/gtags.el
+++ cfg/gtags.el
@@ -1,12 +1,15 @@
 ;; depends on cc-mode because c-mode-map defined there
 (require 'cc-mode)
 
-(setq gtags-suggested-key-mapping t
+(setq gtags-suggested-key-mapping nil
       gtags-auto-update t)
 
 (require 'gtags)
 
-(define-key c-mode-base-map (kbd "M-,")   'gtags-find-tag-from-here)
-(define-key c-mode-base-map (kbd "C-M-,") 'gtags-find-rtag)
-(define-key c-mode-base-map (kbd "M-/")   'gtags-find-pattern)
+(define-key gtags-mode-map (kbd "C-c v") #'gtags-visit-rootdir)
+(define-key gtags-mode-map (kbd "M-,") #'gtags-find-tag-from-here)
+(define-key gtags-mode-map (kbd "M-.") #'gtags-find-tag)
+(define-key gtags-mode-map (kbd "C-M-,") #'gtags-find-rtag)
+(define-key gtags-mode-map (kbd "M-^") #'gtags-pop-stack)
 
+(add-hook 'c-mode-common-hook #'gtags-mode)
