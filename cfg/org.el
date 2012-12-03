b3ee0734266cecb9d03be59ac0ff7a348bbcfc41
Pavel Sokolov <psokolov@synopsys.com>
2012-12-03 20:11:54 +0400

    org: mobile-org support is added

--- cfg/org.el
+++ cfg/org.el
@@ -3,13 +3,18 @@
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org\\(-mode\\)?$" . org-mode))
 
-(setq org-agenda-files '("~/dropbox/org/agenda.org.gpg")
-      org-agenda-window-setup 'current-window)
+(setq
+ org-mobile-directory "~/dropbox/mobile-org"
+ org-agenda-window-setup 'current-window)
 
-(add-hook 'org-mode-hook #'(lambda () 
-			     ;; C-t is reserved by the stumpWM so this
-			     ;; binding will be very handy
-			     (define-key org-mode-map (kbd "C-c t") #'org-todo)
-			     (define-key org-agenda-mode-map (kbd "C-c t") #'org-agenda-todo)))
+(add-hook 'org-mode-hook
+	  #'(lambda () 
+	      ;; C-t is reserved by the stumpWM so this
+	      ;; binding will be very handy
+	      (define-key org-mode-map (kbd "C-c t") #'org-todo)
+	      (define-key org-agenda-mode-map (kbd "C-c t") #'org-agenda-todo)
+	      ;; mobile-org shortcuts
+	      (define-key org-agenda-mode-map (kbd "C-c p") #'org-mobile-push)
+	      (define-key org-agenda-mode-map (kbd "C-c M-p") #'org-mobile-pull)))
 
 ;;-------------------------------------------------------------------------------
