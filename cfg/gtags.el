675e349cf00d287341c52cc41a98cd84eb9a6f96
Pavel Sokolov <psokolov@synopsys.com>
2012-12-07 12:28:13 +0400

    gtags: another shortcut update
    to jump over the code faster :)

--- cfg/gtags.el
+++ cfg/gtags.el
@@ -8,11 +8,12 @@
 
 (define-key gtags-mode-map (kbd "C-c v") #'gtags-visit-rootdir)
 (define-key gtags-mode-map (kbd "M-r") #'gtags-find-rtag)
-(define-key gtags-mode-map (kbd "M-,") #'gtags-find-tag)
-(define-key gtags-mode-map (kbd "M-.") #'gtags-find-rtag)
+(define-key gtags-mode-map (kbd "M-s") #'gtags-find-symbol)
 (define-key gtags-mode-map (kbd "M-^") #'gtags-pop-stack)
 (define-key gtags-mode-map (kbd "M-j") #'gtags-find-tag-from-here)
+(define-key gtags-mode-map (kbd "M-J") #'gtags-find-tag)
 (define-key gtags-select-mode-map (kbd "M-^") #'gtags-pop-stack)
 (define-key gtags-select-mode-map (kbd "j") #'gtags-select-tag)
+(define-key gtags-select-mode-map (kbd "M-j") #'gtags-select-tag-other-window)
 
 (add-hook 'c-mode-common-hook #'gtags-mode)
