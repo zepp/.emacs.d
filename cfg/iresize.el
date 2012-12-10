024915e66522f6ca676c979100f022f6eb3640af
Pavel Sokolov <psokolov@synopsys.com>
2012-12-10 18:46:44 +0400

    iresize: new shortcuts
    finish re-size by the 'enter' key to resemble stumpwm behaviour.

--- cfg/iresize.el
+++ cfg/iresize.el
@@ -1,1 +1,5 @@
 (require 'iresize)
+
+(define-key iresize-mode-map (kbd "RET") #'iresize-mode)
+(define-key iresize-mode-map (kbd "C-f") #'enlarge-window-horizontally)
+(define-key iresize-mode-map (kbd "C-b") #'shrink-window-horizontally)
