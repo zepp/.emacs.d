3b6948bea101309eb04b6979d40f6a6a93fbf017
Pavel Sokolov <psokolov@synopsys.com>
2013-04-18 16:02:16 +0400

    shell: jump routine is fixed
    to consider the prefix argument and open shell in separate window

--- init.el
+++ init.el
@@ -174,19 +174,26 @@ various buffer management routines")
 
 (require 'shell)
 (defun shell-jump ()
+  "opens the shell in the current directory. Opens new window if
+prefix argument is set"
+
   (interactive)
-  (let ((new-shell-buf-name 
+  (let ((new-shell-buf-name
 	 (if (eq major-mode 'dired-mode)
 	     (concat (buffer-name) ":shell")
 	   (let* ((dir (directory-file-name
-			(if buffer-file-name 
+			(if buffer-file-name
 			    (file-name-directory buffer-file-name)
 			  default-directory))))
 	     (concat (car (last (split-string dir "/"))) 
 		     ":shell")))))
-    (unless (get-buffer new-shell-buf-name)
-      (add-to-list 'same-window-buffer-names new-shell-buf-name))
-    (shell new-shell-buf-name )))
+    (if current-prefix-arg
+	(let ((split-height-threshold 0)
+	      (split-width-threshold nil))
+	  (shell new-shell-buf-name))
+      (let ((same-window-buffer-names
+	     (cons new-shell-buf-name same-window-buffer-names)))
+	(shell new-shell-buf-name)))))
 
 ;;-------------------------------------------------------------------------------
 
