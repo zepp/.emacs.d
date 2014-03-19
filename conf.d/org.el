c92664db6a4324fcbbc82c7400df78f303c44083
Pavel Sokolov <psokolov@synopsys.com>
2014-03-19 18:40:54 +0400

    org: clocking setup

--- conf.d/org.el
+++ conf.d/org.el
@@ -1,15 +1,13 @@
 (setq
+ org-clock-persist t
+ org-clock-persist-file (expand-file-name "org/org-clock-save.el" local-conf-dir)
  org-mobile-force-mobile-change t
  org-mobile-directory "~/Dropbox/mobile-org"
  org-id-locations-file (expand-file-name "org/.org-id-locations" local-conf-dir)
  org-agenda-window-setup 'current-window)
 
 (add-hook 'org-mode-hook
-          #'(lambda () 
-              ;; C-t is reserved by the stumpWM so this
-              ;; binding will be very handy
-              (define-key org-mode-map (kbd "C-c t") #'org-todo)
-              (define-key org-agenda-mode-map (kbd "C-c t") #'org-agenda-todo)
+          #'(lambda ()
               (define-key org-agenda-mode-map (kbd "M-n") #'org-agenda-next-item)
               (define-key org-agenda-mode-map (kbd "M-p") #'org-agenda-previous-item)
               (define-key org-agenda-mode-map (kbd "C-M-n") #'org-agenda-next-date-line)
@@ -18,3 +16,4 @@
               (define-key org-agenda-mode-map (kbd "C-c C-p") #'org-mobile-push)
               (define-key org-agenda-mode-map (kbd "C-c C-u") #'org-mobile-pull)))
 
+(org-clock-persistence-insinuate)
