fb9222abac7b9792ee3e3ac6edbde93466060659
Pavel Sokolov <pavel.zepp@gmail.com>
2016-02-05 19:58:23 +0300

    another cleanup

--- conf.d/env.el
+++ conf.d/env.el
@@ -1,29 +1,3 @@
-(if (string= system-type "windows-nt")
-    (w32-register-hot-key [M-tab])
-  (setq
-   ;; disable internal decoder
-   base64-internal-decoding-limit 0
-   base64-internal-encoding-limit 0
+(setenv "GPG_AGENT_INFO" nil)
+(setq browse-url-browser-function 'browse-url-firefox)
 
-   quoted-printable-internal-encoding-limit 0
-   quoted-printable-internal-decoding-limit 0
-
-   ;; using metamail
-   ;; base64-external-encode '("mimencode")
-   ;; base64-external-decode '("mimencode" "-u")
-   ;; quoted-printable-external-encoder '("mimencode" "-q")
-   ;; quoted-printable-external-decoder '("mimencode" "-q" "-u")
-
-   ;; using sharutils
-   base64-external-encode '("uuencode" "-m")
-   base64-external-decode '("uudecode" "-m")
-   quoted-printable-external-encoder '("uuencode")
-   quoted-printable-external-decoder '("uudecode"))
-  (setenv "GPG_AGENT_INFO" nil))
-
-(if (string= (getenv "DESKTOP_SESSION") "stumpwm")
-    (setq
-     browse-url-browser-function 'browse-url-generic
-     browse-url-generic-program "conkeror")
-  (setq
-   browse-url-browser-function 'browse-url-firefox))
--- conf.d/fonts.el
+++ conf.d/fonts.el
@@ -1,24 +1,9 @@
-;; custom-set-faces was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-
 (if (string= system-type "windows-nt")
+    (progn
+      (custom-set-faces
+       '(default ((t (:height 110 :family "Lucida Console")))))
+      (add-to-list 'default-frame-alist '(font . "Lucida Console-11")))
+  (progn
     (custom-set-faces
-     '(default ((t (:height 110 :family "Lucida Console")))))
-  (custom-set-faces
-   '(default ((t (:height 110 :family "Dejavu Sans Mono"))))
-   '(jabber-activity-face ((t (:weight bold))))
-   '(jabber-chat-error ((t (:foreground "dark red" :weight bold))))
-   '(jabber-chat-prompt-foreign ((t (:foreground "grey"))))
-   '(jabber-chat-prompt-local ((t (:foreground "white"))))
-   '(jabber-chat-prompt-system ((t (:foreground "dark green" :weight bold))))
-   '(jabber-chat-text-foreign ((t (:foreground "grey"))))
-   '(jabber-roster-user-away ((t (:foreground "grey" :slant italic))))
-   '(jabber-roster-user-dnd ((t (:foreground "orange4" :slant italic))))
-   '(jabber-roster-user-online ((t (:slant normal))))
-   '(jabber-roster-user-xa ((t (:foreground "grey" :slant italic))))
-   '(jabber-title-medium ((t (:family "dejavu sans mono" :weight bold :foreground "orange3"))))
-   '(jabber-title-small ((t (:family "dejavu sans mono" :weight bold))))))
-
-(add-to-list 'default-frame-alist '(font . "dejavu sans mono-11"))
+     '(default ((t (:height 110 :family "Inconsolata")))))
+    (add-to-list 'default-frame-alist '(font . "Inconsolata-11"))))
