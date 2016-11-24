aea39d3f5ca5820b9a85a700b2c79a796cf0f904
Pavel Zepp <pavel.zepp@gmail.com>
2016-11-24 16:51:57 +0300

    keys: minor update

--- conf.d/helm.el
+++ conf.d/helm.el
@@ -3,14 +3,14 @@
 (require 'helm-buffers)
 (helm-autoresize-mode 1)
 
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-c h")   'helm-command-prefix)
+(global-set-key (kbd "C-x b")   'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "M-x")     'helm-M-x)
+(global-set-key (kbd "M-y")     'helm-show-kill-ring)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 (eval-after-load 'shell
