8fb7c34625439a528b0fee30b1e164e007089c57
Pavel Zepp <pavel.zepp@gmail.com>
2017-11-11 12:39:53 +0300

    minor cc-mode update

--- conf.d/cc.el
+++ conf.d/cc.el
@@ -1,6 +1,6 @@
 (add-hook 'c-mode-common-hook
           #'(lambda ()
+              (setq indent-tabs-mode nil)
               (auto-fill-mode 1)
               (c-toggle-hungry-state 1)
-              (c-toggle-auto-newline -1)
-              (whitespace-mode -1)))
+              (c-toggle-auto-newline -1)))
