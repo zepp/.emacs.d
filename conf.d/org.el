c3b51ba6e6b41197b36d53f37a8cddc30dcc34e4
Pavel Sokolov <pavel.zeppa@yandex.ru>
2023-10-28 22:02:14 +0300

    org: no tags in agenda
    since it breaks layout in case of inherited tags are included

--- conf.d/org.el
+++ conf.d/org.el
@@ -5,6 +5,7 @@
  org-tags-column 0
  org-catch-invisible-edits 'show-and-error
  org-time-stamp-custom-formats '("<%a %d %b %Y>" . "<%a %H:%M %d %b %Y>")
+ org-agenda-remove-tags t
  org-agenda-window-setup 'current-window)
 
 (add-hook 'org-mode-hook #'org-indent-mode)
