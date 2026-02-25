7bfce21873f86c4a97fdd95970a0165324997c23
Pavel Sokolov <pavel.zepp@gmail.com>
2026-02-25 10:46:56 +0300

    garbage-buffer-collector: base point & smaller delay unit
    - base point to measure buffer metrics
    - measure garbage collection delay in minutes rather then hours
    - additional debug messages

--- loadable/garbage-buffer-collector.el
+++ loadable/garbage-buffer-collector.el
@@ -17,8 +17,8 @@
 
 (defgroup garbage-buffer-collector nil "Main group")
 
-(defcustom garbage-buffer-collection-delay 3
-  "Number of hours to delay a garbage buffer collection."
+(defcustom garbage-buffer-collection-delay (* 3 60)
+  "Number of minutes to delay a garbage buffer collection."
   :group 'garbage-buffer-collector
   :type 'natnum)
 
@@ -36,19 +36,25 @@ a garbage collection"
 
 (defconst garbage-buffer-collector-timer
   (let ((timer (timer-create))
-        (seconds (* garbage-buffer-collection-delay 60 60)))
+        (seconds (* garbage-buffer-collection-delay 60)))
     (timer-set-function timer #'garbage-buffer-collect)
     (timer-set-idle-time timer seconds)
     timer)
   "Timer to schedule a garbage buffer collection. It is activated by
 `garbage-buffer-schedule'")
 
+(defvar garbage-buffer-last-base-point nil
+  "base point to measure buffer metrics")
+
 (defun garbage-buffer-collector-set-delay (hours &optional minutes)
-  "Updates a delay to run a garbage buffer collection."
-  (cl-assert (natnump hours))
+  "Updates a delay to run a garbage buffer collection and
+`garbage-buffer-collection-delay' special variable."
+  (cl-assert (and (natnump hours)
+                  (if minutes (natnump minutes) t)))
   (let ((timer garbage-buffer-collector-timer)
-        (seconds (* (+ (* hours 60) (or minutes 0)) 60)))
-    (timer-set-idle-time timer seconds)))
+        (minutes (+ (* hours 60) (or minutes 0))))
+    (timer-set-idle-time timer (* minutes 60))
+    (setq garbage-buffer-collection-delay minutes)))
 
 ;;;###autoload
 (defun garbage-buffer-collector-init ()
@@ -58,7 +64,8 @@ idle."
   (interactive)
   (add-hook 'buffer-list-update-hook
             #'garbage-buffer-schedule)
-  (garbage-buffer-schedule))
+  (garbage-buffer-schedule)
+  (setq garbage-buffer-last-base-point (current-time)))
 
 (defun garbage-buffer-schedule ()
   "Schedules a garbage buffer collection."
@@ -84,20 +91,20 @@ are not not collectible."
 (defmacro garbage-buffer-mem-size (buffers)
   `(apply #'+ (mapcar #'buffer-size ,buffers)))
 
-(defun garbage-buffer-sort (buffers)
+(defun garbage-buffer-sort (buffers time)
   "sorts BUFFERS list accoding to a buffer weight."
   (seq-sort #'(lambda (a b)
                 (> (buffer-size a)
                    (buffer-size b)))
             buffers))
 
-(defun garbage-buffer-reduce (buffers)
+(defun garbage-buffer-reduce (buffers time)
   "Reduces BUFFERS list if one's total memory size is greater then
 `garbage-buffer-memory-threshold'. `garbage-buffer-sort' decides
 which buffers should be released last."
 
   (let* ((threshold garbage-buffer-memory-threshold)
-         (buffers (garbage-buffer-sort buffers))
+         (buffers (garbage-buffer-sort buffers time))
          (total-size (garbage-buffer-mem-size buffers)))
     (when (> total-size threshold)
       (let ((release-at-least (- total-size threshold)))
@@ -109,23 +116,43 @@ which buffers should be released last."
          buffers
          nil)))))
 
+(defun garbage-buffer-emacs-last-active ()
+  "Returns a last moment when Emacs was active"
+  (let ((idle-time (current-idle-time))
+        (current-time (current-time)))
+    (if idle-time
+        (time-subtract current-time idle-time)
+      current-time)))
+
+(defun garbage-buffer-list-to-string (buffers)
+  "Joins BUFFERS names to a string for a debug purposes."
+  (string-join
+   (seq-map #'(lambda (buf)
+                (truncate-string-to-width (buffer-name buf) 20 nil nil t))
+            buffers)
+   " "))
+
 ;;;###autoload
 (defun garbage-buffer-collect (&optional no-reduce)
   "Runs a garbage buffer collection. List of collected buffers is
 reduced by `garbage-buffer-reduce' if NO-REDUCE argument is nil."
   (interactive)
-  (when-let* ((buffers (if garbage-buffer-query
+  (when-let* ((time (garbage-buffer-emacs-last-active))
+              (buffers (if garbage-buffer-query
                            (match-buffers garbage-buffer-query)
                          (buffer-list)))
               (collectible (seq-filter #'garbage-buffer-collectible-p buffers)))
+    (message "last active time - %s" (format-time-string "%F %R" time))
     (message "%i/%i buffers are collectible" (length collectible) (length buffers))
     (when-let* ((reduced (if no-reduce
                              collectible
-                           (garbage-buffer-reduce collectible)))
-                (mem-to-release (garbage-buffer-mem-size reduced)))
+                           (garbage-buffer-reduce collectible time)))
+                (mem-to-release (garbage-buffer-mem-size reduced))
+                (names (garbage-buffer-list-to-string reduced)))
       (mapcar #'kill-buffer reduced)
-      (message "%s bytes of memory is realsed after a garbage buffer collection"
-               mem-to-release)
-      (cancel-timer garbage-buffer-collector-timer))))
+      (message "released buffers: %s" names)
+      (message "%s bytes are realsed" mem-to-release)
+      (cancel-timer garbage-buffer-collector-timer)
+      (setq garbage-buffer-last-base-point time))))
 
 (provide 'garbage-buffer-collector)
